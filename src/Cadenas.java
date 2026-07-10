86af6d0ccaf9410d22fe9303e5c581a307cbece2
Renzo Gomez <renzogomez577@gmail.com>
2026-07-10 14:31:30 -0500

    first commit

---
+++ src/Cadenas.java
@@ -0,0 +1,14 @@
+public class Cadenas {
+    public static void main(String[] args) {
+        System.out.println("*******Chain handle ******");
+        var chain1 = "Hola";
+        System.out.println("chain1 :"+ chain1);
+        var chain2 = new String("Mundo");
+
+        var multiLine = """
+                This is a 
+                    text multi line.
+                """;
+        System.out.println(multiLine);
+    }
+}
